b943b4aabdc9ebb437ab5af004958c84af219d09
pisapa27 <chazz.pisanelli19@alumni.loyola.ca>
2017-05-30 14:11:26 -0400

    Update Killcow.lua

--- Killcow.lua
+++ Killcow.lua
@@ -1,3 +1,21 @@
-local function kill()
+turtle.getItemCount(1)
+for t = 1,6 do
+for x = 1,8 do
+  turtle.forward()
+  turtle.attackDown()
+end
+turtle.getItemCount(1)
+for y = 1,8 do
+  turtle.back()
+  turtle.attackDown()
+end
+turtle.turnLeft()
 turtle.forward()
-turtle.attackDown()
+turtle.turnRight()
+end
+turtle.turnRight()
+turtle.forward()
+turtle.forward()
+turtle.forward()
+turtle.forward()
+turtle.turn Left()
