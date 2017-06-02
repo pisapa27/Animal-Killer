0afa1e10baab81956e293c5369ef34fb9767f22c
pisapa27 <chazz.pisanelli19@alumni.loyola.ca>
2017-06-02 12:14:03 -0400

    Update Killcow.lua

--- Killcow.lua
+++ Killcow.lua
@@ -1,21 +1,27 @@
+--This is code to kill cows in a 6x8 space
+--This is to count the number of items in slot 1 of the turtle's inventory
+local s == 0
 turtle.getItemCount(1)
-for t = 1,6 do
-for x = 1,8 do
+--How my turtle will move and attack
+for t = 1,6 do 
+	for x = 1,8 do
+    turtle.forward()
+    turtle.attackDown()
+  end
+  for y = 1,8 do
+    turtle.back()
+    turtle.attackDown()
+  end
+--I wanted to have my turtle stop here by having a certain number of items in slot 1 using turtle.getItemCount
+  turtle.turnLeft()
   turtle.forward()
-  turtle.attackDown()
+  turtle.turnRight()
 end
-turtle.getItemCount(1)
-for y = 1,8 do
-  turtle.back()
-  turtle.attackDown()
-end
+--This is to get my turtle back to its starting position after having completed its route of the area
+turtle.turnRight()
+turtle.forward()
+turtle.forward()
+turtle.forward()
+turtle.forward()
+turtle.forward()
 turtle.turnLeft()
-turtle.forward()
-turtle.turnRight()
-end
-turtle.turnRight()
-turtle.forward()
-turtle.forward()
-turtle.forward()
-turtle.forward()
-turtle.turn Left()
