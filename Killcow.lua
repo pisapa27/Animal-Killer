8fc390fb1fa3122dd6d7ff52b92ca54e364fff7b
pisapa27 <chazz.pisanelli19@alumni.loyola.ca>
2017-06-08 11:56:35 -0400

    Create Killcow.lua

--- Killcow.lua
+++ Killcow.lua
@@ -2,29 +2,28 @@
 --Variable to know how many items are in slot 1
 local ItemCount = turtle.getItemCount(1)
 local move = 0
---How my turtle will move and attack
-while ItemCount =<10 then
-  for t = 1,6 do 
-    for x = 1,8 do
-      turtle.forward()
-      turtle.attackDown()
-    end
+--How my turtle counts when to stop and when to continue
+while ItemCount <=15 and move <6 do 
+--How my turtle moves and attacks  
+  for x = 1,8 do 
+    turtle.forward()
+    turtle.attackDown()
+  end
     for y = 1,8 do
       turtle.back()
       turtle.attackDown()
     end
 --This is to count items in slot 1        
-    turtle.getItemCount(1)            
---I wanted to have my turtle stop here by having a certain number of items in slot 1 using turtle.getItemCount
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.turnRight()
-    move == move + 1
-  end
+  ItemCount = turtle.getItemCount(1)
+--This brings my turtle into the next row  
+  turtle.turnLeft()    
+  turtle.forward()
+  turtle.turnRight()
+  move = move + 1
 end
---This is to get my turtle back to its starting position after having completed its route of the area
+--This is to get my turtle back to its starting position by counting how mant times it killed a row
 turtle.turnRight()
-for m = 1 to move do
+for m = 1 , move do
   turtle.forward()
 end
 turtle.turnLeft()
